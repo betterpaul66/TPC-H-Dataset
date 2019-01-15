b3f435862d33be286cb60dc9bca4a9764083586c
Ng Kwok Ping, Paul <46708879+betterpaul66@users.noreply.github.com>
2019-01-15 11:44:30 +0100

    Update JoinQuery.cpp

--- src/JoinQuery.cpp
+++ src/JoinQuery.cpp
@@ -28,38 +28,11 @@ size_t JoinQuery::avg(std::string segmentParam)
    std::string oline;
    std::string cline;
    std::string lline;
-   //std::string olinevalue;
-   //std::string clinevalue;
-   //std::string llinevalue;
 
-  
-
-   
-
-   //std::vector<int> Custkey;
-
-   //std::vector<int> Orderkey;
-
-   //std::vector<int>::iterator OrderkeyIt;
-
-   //std::vector<std::pair<int, int>> Ordertable;
-   //std::vector<std::pair<int, int>>::iterator OrdertableIt;
-   //std::vector<std::pair<int, double>> linetable;
-   //std::vector<std::pair<int, double>>::iterator linetableIt;
-
-   //std::string keyl = "";
-   //std::string valuel = "";
-
-   // std::string keyc = "";
-   //std::string valuec = "";
-   // 
-   //std::string valueo = "";
-
-   //std::istringstream temp;
 
   std::ifstream c(customer);
   std::unordered_set<std::string> Custkey;
-   //std::vector<int>::iterator CustkeyIt;
+   
   while (std::getline(c, cline, '\n')) {
 		
 	  if (cline.find(segmentParam) != std::string::npos)
@@ -71,10 +44,6 @@ size_t JoinQuery::avg(std::string segmentParam)
 
    }
 
-	  //for (auto it = Custkey.begin(); it != Custkey.end();++it)
-	  //{
-		 // std::cout << *it << std::endl;
-	  //}
 
  
 
@@ -82,40 +51,36 @@ size_t JoinQuery::avg(std::string segmentParam)
    std::ifstream o(order);
    std::unordered_set<std::string> Orderkey;
   while (std::getline(o, oline, '\n')) {
-      //std::cout << oline << std::endl;
+      
 	  std::size_t o_oize1 = oline.find("|", 0);
     
       std::string keyo = oline.substr(0, o_oize1);
-         // std::cout << keyo << std::endl; 
+         
       std::size_t o_oize2 = oline.find("|", o_oize1+1);
       std::string keyc = oline.substr(o_oize1 + 1, o_oize2-o_oize1-1);
-      //std::cout << keyc << std::endl; 
+       
       if (Custkey.find(keyc) != Custkey.end())
 	  { 
 		  Orderkey.insert(keyo);
-          //std::cout << keyo << std::endl;
+          
          
 	  }
    }
 
-  // for (auto it = Orderkey.begin(); it != Orderkey.end(); ++it)
-  //{
-  // std::cout << *it << std::endl;
-  //}
-  // 
+ 
     std::ifstream l(lineitem);
     while (std::getline(l, lline, '\n')) {
 
-		//std::cout << lline << std::endl;
+		
 		std::size_t l_lize1 = lline.find("|", 0);
         std::string keyo = lline.substr(0, l_lize1); 
-		//std::cout << keyo << std::endl;
+		
 		std::size_t l_lize2 = lline.find("|", l_lize1+1);
         std::size_t l_lize3 = lline.find("|", l_lize2+1);
         std::size_t l_lize4 = lline.find("|", l_lize3+1);
         std::size_t l_lize5 = lline.find("|", l_lize4 + 1);
         std::string valuel = lline.substr(l_lize4 + 1, l_lize5 - l_lize4 - 1);
-        //std::cout << valuel << std::endl;
+        
         if (Orderkey.find(keyo) != Orderkey.end()) { 
 			sumq += std::stod(valuel);
            ++j;
